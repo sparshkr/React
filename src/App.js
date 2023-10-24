721ed10da6706e7322d1d8b3d8ca46929935db93
Sparsh <sparshkumar910@gmail.com>
2023-10-24 11:13:10 +0530

    Going on to tailwind css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
+
 import "./App.css";
 import Employee from "./components/Employee";
+import { useState } from "react";
 
 function App() {
+  const [role,setRole] = useState('dev');
+  
   console.log("List of Employees");
   const showEmployee = true;
   return (
     <div className="App">
       {console.log('Inside the return')}
       {showEmployee ? (
+        <>
+        <input type="text" onChange={(e) => {
+          console.log(e.target.value);
+          setRole(e.target.value);
+        }} />
         <p>
           <Employee name="Sparsh" role="Intern"/>
-          <Employee name="Abby" />
+          <Employee name="Abby" role={role}/>
           <Employee name="Caleb" role="SDE"/>
           
         </p>
+        </>
       ) : (
         <p>You cannot view the Employees</p>
       )}
